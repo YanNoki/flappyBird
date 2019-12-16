dd4d4941ceb177fa51679c4c93f6b781bf927e5b
yy <yy@qq.com>
2019-12-16 18:02:07 +0800

    第二天结束
    画背景图,地板,水管,小鸟,小鸟飞行,自由落体,撞击

---
+++ js/runtime/UpPipe.js
@@ -0,0 +1,17 @@
+import { Pipe } from "./Pipe.js";
+import { Sprite } from "../base/Sprite.js";
+
+
+export class UpPipe extends Pipe{
+  constructor(top){
+    const img = Sprite.getImage('upPipe');
+    console.log(img);
+    super(img,top);
+  }
+
+
+  draw(){
+    this.y = this.top - this.height;
+    super.draw();
+  }
+}
